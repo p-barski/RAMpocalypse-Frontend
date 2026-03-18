4046379883b8fba3a270897d73a8ae1c15183066
p-barski <patryk.kubarski@protonmail.com>
2026-03-18 17:23:46 +0100

    added test specific eslint ignores

--- eslint.config.js
+++ eslint.config.js
@@ -53,4 +53,12 @@ export default defineConfig([
       ...prettier.rules,
     },
   },
+  {
+    files: ['**/*.test.{ts,tsx}', '**/*.spec.{ts,tsx}'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
 ]);
