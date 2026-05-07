12f798b812769f285d5f6db4a2536a477a3fcd34
p-barski <patryk.kubarski@protonmail.com>
2026-05-07 17:34:30 +0200

    Added chat cooldown

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -3,6 +3,29 @@ import { ChatMessageTypeValue, type ChatMessageServer, type ChatMessageType } fr
 import { convertChatMessageFromServer, dateToChatTimestamp, profanityFilter, type ChatMessage } from './utils';
 import './Chat.css';
 
+class ChatCooldown {
+  private readonly MSG_COOLDOWN_MS = 1000;
+  private readonly ONE_MINUTE_MS = 60000;
+  private readonly MAX_MSGS_PER_MINUTE = 30;
+  private readonly previousTimestamps = new Float64Array(this.MAX_MSGS_PER_MINUTE);
+  private currentTimestampIndex = 0;
+  private lastMsgSentTimestamp = 0;
+
+  getRemainingCooldown(): number {
+    const now = Date.now();
+    const secondCooldownRemaining = this.MSG_COOLDOWN_MS - (now - this.lastMsgSentTimestamp);
+    const minuteCooldownRemaining = this.ONE_MINUTE_MS - (now - this.previousTimestamps[this.currentTimestampIndex]);
+    return Math.max(0, secondCooldownRemaining, minuteCooldownRemaining);
+  }
+
+  updateCooldowns(): void {
+    const now = Date.now();
+    this.previousTimestamps[this.currentTimestampIndex] = now;
+    this.currentTimestampIndex = (this.currentTimestampIndex + 1) % this.MAX_MSGS_PER_MINUTE;
+    this.lastMsgSentTimestamp = now;
+  }
+}
+
 interface ChatProps {
   canvasRef: React.RefObject<HTMLCanvasElement | null>;
   serverUrl: string;
@@ -12,6 +35,8 @@ interface ChatProps {
 
 function Chat({ canvasRef, serverUrl, sendMessage, registerOnMessageReceived }: ChatProps) {
   const isChatActiveRef = useRef(false);
+  const chatCooldownRef = useRef<ChatCooldown | null>(null);
+  const [onCooldown, setOnCooldown] = useState(false);
   const [isChatActive, setIsChatActive] = useState(false);
   const [activeTab, setActiveTab] = useState<ChatMessageType>(ChatMessageTypeValue.Global);
   const [globalMessages, setGlobalMessages] = useState<ChatMessage[]>([]);
@@ -28,11 +53,19 @@ function Chat({ canvasRef, serverUrl, sendMessage, registerOnMessageReceived }: 
   const handleSendMessage = useCallback(() => {
     const message = messageInputRef.current?.value;
     if (message === undefined || !message.trim()) return;
+    const cooldown = chatCooldownRef.current?.getRemainingCooldown();
+    if (cooldown !== undefined && cooldown > 0) {
+      setOnCooldown(true);
+      setTimeout(() => setOnCooldown(false), cooldown);
+      return;
+    }
+    chatCooldownRef.current?.updateCooldowns();
     sendMessage(profanityFilter.clean(message), activeTab);
     messageInputRef.current!.value = '';
   }, [sendMessage, activeTab]);
 
   useEffect(() => {
+    chatCooldownRef.current = new ChatCooldown();
     const onMessageReceived = (msg: ChatMessageServer) => {
       const msgUI = convertChatMessageFromServer(msg);
       switch (msg.type) {
@@ -113,6 +146,7 @@ function Chat({ canvasRef, serverUrl, sendMessage, registerOnMessageReceived }: 
       </div>
       <div className="chat-input-container">
         <input
+          className={`${onCooldown ? 'on-cooldown' : ''}`}
           ref={messageInputRef}
           type="text"
           maxLength={100}
@@ -121,7 +155,7 @@ function Chat({ canvasRef, serverUrl, sendMessage, registerOnMessageReceived }: 
           }}
           placeholder="Type a message..."
         />
-        <button onClick={handleSendMessage}>⬅️</button>
+        <button onClick={handleSendMessage}>{onCooldown ? '❌' : '⬅️'}</button>
       </div>
     </div>
   );
